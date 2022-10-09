fe67196b2c2ad5e63569b0d20ca3975c10253089
bham <bham@bham>
2022-10-09 12:39:13 +0100

    added comments for initial test cases

--- test_bst.c
+++ test_bst.c
@@ -5,23 +5,31 @@
 
 int main() {
 
+    printf("Original test case 1: testing removeNode\n");
     Node *a, *b;
     a = addNode(NULL, 42);
     b = removeNode(a, 42);
     assert(b == NULL);
+    printf("test passed\n");
 
+    printf("Original test case 2: testing a 1 2 tree\n");
     a = addNode(NULL, 1);
     b = addNode(a, 2);
-
     displaySubtree(a);
+    printf("-\nthe output should be\n2\n1\n-\n");
 
+    printf("Original test case 3: testing numberLeaves\n");
     int c = numberLeaves(a);
     assert(c == 1);
+    printf("test passed\n");
 
+    printf("Original test case 3: testing nnodeDepth\n");
     c = nodeDepth(a, b);
     assert(c == 1);
+    printf("test passed\n");
 
-    a = removeSubtree(a, 2);
+    printf("Removing the entire tree to prevent memory leak\n");
+    a = removeSubtree(a, 1);
 
     return 0;
 }
